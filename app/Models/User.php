5a9e42a357e8aab06c4774e723fc015f5a13b284
ZINEB TALGHMET <zinebtalghmet2022@gmail.com>
2026-06-23 12:09:34 +0100

    chore: init laravel project with sanctum and api scaffolding

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,19 +9,30 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory,HasApiTokens, Notifiable;
 
     /**
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
      */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',        
+        'remember_token',  
+    ];
     protected function casts(): array
     {
         return [
