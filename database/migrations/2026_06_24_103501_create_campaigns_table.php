9ad08f665e92081bdc622e2d881d7c2ae258859e
ZINEB TALGHMET <zinebtalghmet2022@gmail.com>
2026-06-24 12:45:59 +0100

    feat(campaigns): create campaigns migration

---
+++ database/migrations/2026_06_24_103501_create_campaigns_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('campaigns', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->string('tone');
+            $table->integer('max_hashtags')->default(1);
+            $table->integer('max_characters')->default(280);
+            $table->string('target_audience')->nullable();
+            $table->text('style_rules')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('campaigns');
+    }
+};
