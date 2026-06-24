bab7311e3b2ccd27a11663d38860bde108f630a0
ZINEB TALGHMET <zinebtalghmet2022@gmail.com>
2026-06-24 12:47:04 +0100

    feat(campaigns): add CampaignResource JSON output

---
+++ app/Http/Resources/CampaignResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CampaignResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+    return [
+        'id'              => $this->id,
+        'name'            => $this->name,
+        'tone'            => $this->tone,
+        'max_hashtags'    => $this->max_hashtags,
+        'max_characters'  => $this->max_characters,
+        'target_audience' => $this->target_audience,
+        'style_rules'     => $this->style_rules,
+        'created_at'      => $this->created_at->format('d/m/Y H:i'),
+    ];
+
+    }
+}
