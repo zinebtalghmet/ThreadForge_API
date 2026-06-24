192751eb2108fbe24602f8eed28b534a261cf20d
ZINEB TALGHMET <zinebtalghmet2022@gmail.com>
2026-06-24 12:46:45 +0100

    feat(campaigns): add CampaignRequest form validation

---
+++ app/Http/Requests/CampaignRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CampaignRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'            => ['required', 'string', 'max:255'],
+            'tone'            => ['required', 'string', 'max:100'],
+            'max_hashtags'    => ['sometimes', 'integer', 'min:0', 'max:10'],
+            'max_characters'  => ['sometimes', 'integer', 'min:1', 'max:280'],
+            'target_audience' => ['nullable', 'string', 'max:255'],
+            'style_rules'     => ['nullable', 'string'],
+        ];
+    }
+}
